e220dafd294ad2331db5a9b3272721ce6699870e
Mateus Vega <154934279+MateusVega@users.noreply.github.com>
2024-11-28 14:19:46 -0300

    Update and rename ElementClassifier.js to ElementClassifier.mjs

---
+++ ElementClassifier.mjs
@@ -0,0 +1,23 @@
+(function (global, factory) {
+    if (typeof module === "object" && typeof module.exports === "object") {
+        module.exports = factory(); // For Node.js
+    } else {
+        global.ElementClassifier = factory(); // For browsers
+    }
+})(this, function () {
+    const AvatarGenerator = {
+        classify(elemento): function () {
+            elemento = elemento.tagName
+            if (elemento == 'H1' || elemento == 'H2' || elemento == 'H3' || elemento == 'H4' || elemento == 'H5' || elemento == 'H6' || elemento == 'P'){
+                return 'Text'
+            } else if(elemento == 'BODY' || elemento == 'DIV' || elemento == 'SPAN' || elemento == 'MAIN' || elemento == 'HEADER' || elemento == 'NAV' || elemento == 'ASIDE'){
+                return 'Div'
+            } else if(elemento == 'IMG'){
+                return 'Img'
+            } else if(elemento == 'A'){
+                return 'Link'
+            }
+        },
+    };
+    return ElementClassifier;
+});
